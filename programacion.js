e728fc2fd94134a73cc0596b20dd2970d4c7c65d
Luis Manuel <luismtapia@github.com>
2024-10-16 11:39:27 -0600

    update

--- programacion.js
+++ programacion.js
@@ -38,3 +38,123 @@ document.querySelectorAll('.button-animate').forEach(button => {
     });
 
 });
+
+
+
+// function classToggle() {
+//     this.classList.toggle('class1');
+//     this.classList.toggle('class2');
+// }
+
+// document.querySelector('#elmenu').addEventListener('click', classToggle);
+
+
+function classToggle() {
+    var menu = document.getElementById("menu");
+    var svg1 = document.getElementById("svgicon1");
+    var svg2 = document.getElementById("svgicon2");
+    
+
+    svg1.classList.toggle('block');
+    svg1.classList.toggle('hidden');
+
+    svg2.classList.toggle('block');
+    svg2.classList.toggle('hidden');
+
+    menu.classList.toggle('close');
+    menu.classList.toggle('open');
+
+    this.classList.toggle('setopentrue');
+    this.classList.toggle('setopenfalse');
+
+}
+
+
+document.querySelector('#setopen').addEventListener('click', classToggle);
+
+
+// MODALS
+
+const openEls = document.querySelectorAll("[data-open]");
+const closeEls = document.querySelectorAll("[data-modal-hide]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const el of closeEls) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", e => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function classToggleModal(modal) {
+//     var defaultmodal = document.getElementById("default-modal");
+    
+
+//     defaultmodal.classList.toggle('hidden');
+//     defaultmodal.classList.toggle('block');
+
+//     // this.classList.toggle('setopentrue');
+//     // this.classList.toggle('setopenfalse');
+
+// }
+
+// function classToggleModalQuiropodia(modal) {
+//     var defaultmodal = document.getElementById("modal-quiropodia");
+    
+
+//     defaultmodal.classList.toggle('hidden');
+//     defaultmodal.classList.toggle('block');
+
+// }
+
+
+
+// function onClose() {
+//     var defaultmodal = document.getElementById("default-modal");
+//     var modalQuiropodia = document.getElementById("modal-quiropodia");
+//     defaultmodal.classList.toggle('hidden');
+//     modalQuiropodia.classList.toggle('hidden');
+
+// }
+
+// document.querySelector('#show-modal').addEventListener('click', classToggleModal);
+// document.querySelector('#onClose').addEventListener('click', classToggleModal);
+// document.querySelector('#onAcept').addEventListener('click', classToggleModal);
+
+// document.querySelector('#show-modal-quiropodia').addEventListener('click', classToggleModalQuiropodia);
+
+
+
